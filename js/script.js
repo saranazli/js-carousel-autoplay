ac1478a112cc41facf9cd06d1d952360e4493199
s4r44d43 <sararez@live.it>
2024-03-06 11:40:48 +0100

    [start] repo [add] setinterval

---
+++ js/script.js
@@ -0,0 +1,83 @@
+
+const carouselImgs = [
+  './img/01.webp',
+  './img/02.webp',
+  './img/03.webp',
+  './img/04.webp',
+  './img/05.webp'
+];
+
+console.log(carouselImgs);
+
+const carousel = document.querySelector('.carousel');
+
+const smallCarousel = document.querySelector('.small-carousel');
+
+console.log(carousel);
+
+for(i = 0; i < carouselImgs.length; i++){
+
+  const insImg = carouselImgs[i];
+
+  console.log (insImg);
+
+  carousel.innerHTML += `<img class="conteiner-img single none" src="${insImg}"> `
+
+  smallCarousel.innerHTML += `<img class="conteiner-small-img single-active" src="${insImg}"> `
+};
+
+const imgCollection = document.getElementsByClassName('single');
+
+const imgSmall = document.getElementsByClassName('single-active');
+
+const btnUp = document.querySelector('.top');
+
+const btnDown = document.querySelector('.bottom');
+
+console.log(btnDown, btnUp);
+
+let counter = 0 ;
+
+imgCollection[0].classList.remove('none');
+
+btnUp.classList.add('none');
+
+btnUp.addEventListener('click', next);
+
+btnDown.addEventListener('click', pre);
+
+// AUTO 
+
+setInterval(next, 1000)
+
+
+// FUNCTION 
+
+
+function next(){
+
+  imgCollection[counter++].classList.add('none');
+
+  imgCollection[counter].classList.remove('none');
+
+  if(counter === imgCollection.length - 1 ){
+    btnDown.classList.add('none');
+  }
+  
+  btnUp.classList.remove('none');  
+
+};
+
+function pre(){
+
+  imgCollection[counter--].classList.add('none');
+
+  imgCollection[counter].classList.remove('none');
+
+  if(counter === 0){
+    btnUp.classList.add('none');
+  }
+
+  btnDown.classList.remove('none');
+
+};
